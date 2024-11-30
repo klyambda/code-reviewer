4b763e72f06b46c99e930c7f8d633ea1e4204711
klyambda <PermiakovAA@mpei.ru>
2024-11-30 17:10:20 +0300

    add logg

--- back/resources/project_analyze.py
+++ back/resources/project_analyze.py
@@ -1,3 +1,4 @@
+from loguru import logger
 from bson import ObjectId
 from bson.errors import InvalidId
 from flask_restful import Resource
@@ -23,6 +24,7 @@ class ProjectAnalyze(Resource):
 
         project_manager = ProjectManager()
         structure_tree = project_manager.format_tree(project.get("structure", {}))
+        logger.debug(structure_tree)
 
         evraz_manager = EvrazManager()
         structure_answer = evraz_manager.generate_structure_answer(structure_tree)
