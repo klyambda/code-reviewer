e6d45b6032d1d38e72a49dfaa98483aa56ce7170
klyambda <PermiakovAA@mpei.ru>
2024-11-30 23:14:58 +0300

    f

--- back/resources/answer.py
+++ back/resources/answer.py
@@ -10,11 +10,13 @@ class ProjectAnswer(Resource):
         try:
             project_id = ObjectId(project_id)
         except InvalidId:
-            return {"message": f"No file with id {project_id}"}, 400
+            return {"message": f"No project with id {project_id}"}, 400
 
         project = col_projects.find_one({"_id": project_id}, {"answer": 1})
-        if project is None or "answer" not in project:
-            return {"message": f"No answer with id {project}"}, 400
+        if project is None:
+            return {"message": f"No answer with id {project_id}"}, 400
+        if "answer" not in project:
+            return {"message": f"No answer yet for project with id {project_id}"}, 400
 
         return project["answer"], 200
 
