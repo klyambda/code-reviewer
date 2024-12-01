30aa6b001cf7f2ca4e7ecd56435b43b2403d8c16
dimka070798 <49036025+dimka070798@users.noreply.github.com>
2024-12-01 06:43:59 +0300

    а

--- back/app.py
+++ back/app.py
@@ -17,7 +17,7 @@ app.config["RESTFUL_JSON"] = {"cls": CustomJSONEncoder}
 CORS(app)
 
 api = Api(app)
-api.add_resource(PipelineV1, "/v1/pipeline")
+api.add_resource(PipelineV1, "/v1/pipeline/<project_id>")
 api.add_resource(PipelineV2, "/v2/pipeline/<project_id>")
 api.add_resource(FileAnswer, "/answers/files/<file_id>")
 api.add_resource(ProjectAnswer, "/answers/projects/<project_id>")
